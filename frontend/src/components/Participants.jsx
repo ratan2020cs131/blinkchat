197a5257c7573d6d0c57302e5bf49bfcbec538be
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-02-18 20:21:32 +0530

    app init phase 1

--- frontend/src/components/Participants.jsx
+++ frontend/src/components/Participants.jsx
@@ -1,11 +1,30 @@
 'use client'
 import { Stack, Typography, Button } from '@mui/material'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './style.module.css';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 const Participants = () => {
+  const router = useRouter();
+  const [isOnline, setIsOnline] = useState(true); // Assuming online by default
+
+  useEffect(() => {
+    const handleOnlineStatus = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    handleOnlineStatus();
+
+    window.addEventListener('online', handleOnlineStatus);
+    window.addEventListener('offline', handleOnlineStatus);
+
+    return () => {
+      window.removeEventListener('online', handleOnlineStatus);
+      window.removeEventListener('offline', handleOnlineStatus);
+    };
+  }, []);
 
   const handleCopyLink = async () => {
     try {
@@ -22,6 +41,7 @@ const Participants = () => {
 
   return (
     <Stack sx={{ alignItems: 'center', userSelect: 'none' }}>
+      {!isOnline && <Typography sx={{ fontSize: '17px', fontWeight: 600, width: '100%', lineHeight: '50px', color: '#fff', textAlign: 'center', background: 'tomato' }}>No internet</Typography>}
       <Typography sx={{ color: '#fff', padding: '10px 0', fontWeight: 900, fontSize: '20px', color: '#1976d2' }} className={`${styles.fontFamily}`}>Participants</Typography>
       <Stack>
         <Typography className={`${styles.fontFamily}`} sx={{ ":hover": { backgroundColor: '#1f1f1f' }, color: '#fff', padding: '2px 0', fontWeight: 600, fontSize: '18px', padding: '5px 30px', borderRadius: '8px' }}>Ratan</Typography>
@@ -39,6 +59,7 @@ const Participants = () => {
         <Button variant='outlined' color="error"
           sx={{ padding: '5px 20px', textTransform: 'capitalize' }}
           className={`${styles.fontFamily}`}
+          onClick={() => router.push('/')}
         >
           Delete Room
         </Button>
--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -5,8 +5,8 @@ import Chat from './Chat'
 
 const Room = () => {
   return (
-    <Stack sx={{ width: '100%', height:'98%'}} direction={"row"}>
-      <Stack sx={{width:'25%', minWidth: 'fit-content', padding: '10px 30px', borderRight: '1px solid #a0a0a050', height: '100%' }}>
+    <Stack sx={{ width: '100%', height:'100%'}} direction={"row"}>
+      <Stack sx={{width:'25%', minWidth: 'fit-content', padding: '0px', borderRight: '1px solid #a0a0a050', height: '100%' }}>
         <Participants />
       </Stack>
       <Box sx={{ width: '100%', paddingBottom:'10px'}}>
