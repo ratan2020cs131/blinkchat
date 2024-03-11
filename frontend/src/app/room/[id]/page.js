ecb2e44718c4e9959b72996eed2d6fc77c1b3a57
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-03-11 22:25:18 +0530

    metadata added

--- frontend/src/app/room/[id]/page.js
+++ frontend/src/app/room/[id]/page.js
@@ -2,6 +2,11 @@ import Room from '@/components/Room'
 import React from 'react'
 import styles from '@/app/page.module.css'
 
+export const metadata = {
+    title: "Blinkchat",
+    description: "Join this room to chat",
+};
+
 const page = ({ params }) => {
     return (
         <main className={styles.main}>
